743dc1a54afd6730fe09aaba778c77bf7e5bd003
Suresh Kumar Gadi <gadisureshkumar@gmail.com>
2019-07-20 08:41:13 -0500

    Generating and pushing into server. Also deploys functions

--- public/js/common.js
+++ public/js/common.js
@@ -8,3 +8,8 @@ function findGetParameter(parameterName) {
     }
     return result;
 }
+
+Object.filter = (obj, predicate) => 
+    Object.keys(obj)
+          .filter( key => predicate(obj[key]) )
+          .reduce( (res, key) => (res[key] = obj[key], res), {} );
