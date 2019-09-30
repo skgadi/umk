8628383d5d98f8692c833db2c233c55a5605c792
Suresh Kumar Gadi <gadisureshkumar@gmail.com>
2019-09-30 17:04:59 -0500

    Automatic push

--- docs/js/001-graph.js
+++ docs/js/001-graph.js
@@ -19,11 +19,12 @@ var Graph = function (container) {
     this.vStyle["arcSize"] = 0;
     this.vStyle["absoluteArcSize"] = 1;
     this.vStyle["rounded"] = 1;
+    this.vStyle["strokeWidth"] = 0;
     this.eStyle["edgeStyle"] = "orthogonalEdgeStyle";
     this.eStyle["strokeWidth"] = 1;
     this.eStyle["targetJettySize"] = 25;
     this.eStyle["shadow"] = false;
-    //this.eStyle["endArrow"] = "none";
+    this.eStyle["endArrow"] = "none";
     this.eStyle["fontColor"] = "#000";
     this.eStyle["verticalAlign"] = "top";
     this.eStyle["overflow"] = "width";
@@ -505,6 +506,15 @@ var Graph = function (container) {
         else return cell.value;
     }
 
+    //Handling context icons
+    this.createHandler = function (state) {
+        if (state != null &&
+            this.model.isVertex(state.cell)) {
+            return new mxVertexToolHandler(state);
+        }
+
+        return mxGraph.prototype.createHandler.apply(this, arguments);
+    };
 };
 mxUtils.extend(Graph, mxGraph);
 
@@ -598,3 +608,139 @@ function setCaption(Cell, value) {
         }
     }
 }
+
+//Context icons
+// Defines a subclass for mxVertexHandler that adds a set of clickable
+// icons to every selected vertex.
+function mxVertexToolHandler(state) {
+    mxVertexHandler.apply(this, arguments);
+};
+
+mxVertexToolHandler.prototype = new mxVertexHandler();
+mxVertexToolHandler.prototype.constructor = mxVertexToolHandler;
+
+mxVertexToolHandler.prototype.domNode = null;
+
+mxVertexToolHandler.prototype.init = function () {
+    mxVertexHandler.prototype.init.apply(this, arguments);
+
+    // In this example we force the use of DIVs for images in IE. This
+    // handles transparency in PNG images properly in IE and fixes the
+    // problem that IE routes all mouse events for a gesture via the
+    // initial IMG node, which means the target vertices 
+    this.domNode = document.createElement('div');
+    this.domNode.style.position = 'absolute';
+    this.domNode.style.whiteSpace = 'nowrap';
+
+    // Workaround for event redirection via image tag in quirks and IE8
+    function createImage(src) {
+        if (mxClient.IS_IE && !mxClient.IS_SVG) {
+            var img = document.createElement('div');
+            img.style.backgroundImage = 'url(' + src + ')';
+            img.style.backgroundPosition = 'center';
+            img.style.backgroundRepeat = 'no-repeat';
+            img.style.display = (mxClient.IS_QUIRKS) ? 'inline' : 'inline-block';
+
+            return img;
+        } else {
+            return mxUtils.createImage(src);
+        }
+    };
+
+    // Delete
+    var img = createImage('images/delete2.png');
+    img.setAttribute('title', 'Delete');
+    img.style.cursor = 'pointer';
+    img.style.width = '16px';
+    img.style.height = '16px';
+    mxEvent.addGestureListeners(img,
+        mxUtils.bind(this, function (evt) {
+            // Disables dragging the image
+            mxEvent.consume(evt);
+        })
+    );
+    mxEvent.addListener(img, 'click',
+        mxUtils.bind(this, function (evt) {
+            this.graph.removeCells([this.state.cell]);
+            mxEvent.consume(evt);
+        })
+    );
+    this.domNode.appendChild(img);
+
+    // Size
+    var img = createImage('images/fit_to_size.png');
+    img.setAttribute('title', 'Resize');
+    img.style.cursor = 'se-resize';
+    img.style.width = '16px';
+    img.style.height = '16px';
+    mxEvent.addGestureListeners(img,
+        mxUtils.bind(this, function (evt) {
+            this.start(mxEvent.getClientX(evt), mxEvent.getClientY(evt), 7);
+            this.graph.isMouseDown = true;
+            this.graph.isMouseTrigger = mxEvent.isMouseEvent(evt);
+            mxEvent.consume(evt);
+        })
+    );
+    this.domNode.appendChild(img);
+
+    // Move
+    var img = createImage('images/plus.png');
+    img.setAttribute('title', 'Move');
+    img.style.cursor = 'move';
+    img.style.width = '16px';
+    img.style.height = '16px';
+    mxEvent.addGestureListeners(img,
+        mxUtils.bind(this, function (evt) {
+            this.graph.graphHandler.start(this.state.cell,
+                mxEvent.getClientX(evt), mxEvent.getClientY(evt));
+            this.graph.graphHandler.cellWasClicked = true;
+            this.graph.isMouseDown = true;
+            this.graph.isMouseTrigger = mxEvent.isMouseEvent(evt);
+            mxEvent.consume(evt);
+        })
+    );
+    this.domNode.appendChild(img);
+
+    // Connect
+    var img = createImage('images/check.png');
+    img.setAttribute('title', 'Connect');
+    img.style.cursor = 'pointer';
+    img.style.width = '16px';
+    img.style.height = '16px';
+    mxEvent.addGestureListeners(img,
+        mxUtils.bind(this, function (evt) {
+            var pt = mxUtils.convertPoint(this.graph.container,
+                mxEvent.getClientX(evt), mxEvent.getClientY(evt));
+            this.graph.connectionHandler.start(this.state, pt.x, pt.y);
+            this.graph.isMouseDown = true;
+            this.graph.isMouseTrigger = mxEvent.isMouseEvent(evt);
+            mxEvent.consume(evt);
+        })
+    );
+    this.domNode.appendChild(img);
+
+    this.graph.container.appendChild(this.domNode);
+    this.redrawTools();
+};
+
+mxVertexToolHandler.prototype.redraw = function () {
+    mxVertexHandler.prototype.redraw.apply(this);
+    this.redrawTools();
+};
+
+mxVertexToolHandler.prototype.redrawTools = function () {
+    if (this.state != null && this.domNode != null) {
+        var dy = (mxClient.IS_VML && document.compatMode == 'CSS1Compat') ? 20 : 4;
+        this.domNode.style.left = (this.state.x + this.state.width - 56) + 'px';
+        this.domNode.style.top = (this.state.y + this.state.height + dy) + 'px';
+    }
+};
+
+mxVertexToolHandler.prototype.destroy = function (sender, me) {
+    mxVertexHandler.prototype.destroy.apply(this, arguments);
+
+    if (this.domNode != null) {
+        this.domNode.parentNode.removeChild(this.domNode);
+        this.domNode = null;
+    }
+};
