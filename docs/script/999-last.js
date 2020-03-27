0e772fac22f1bd6d29ffbe2e428ec5e95ed7fa57
Suresh Kumar Gadi <skgadi@users.noreply.github.com>
2020-03-27 05:14:10 -0600

    Automatic push

--- docs/script/999-last.js
+++ docs/script/999-last.js
@@ -1,1 +1,29 @@
-suspendUserInterface.suspendInterface()
+launchFullscreen(document.documentElement);
+suspendUserInterface.suspendInterface();
+
+function launchFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+    }
+}
+
+function exitFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    }
+}
+
+function dumpFullscreen() {
+    console.log("document.fullscreenElement is: ", document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement);
+    console.log("document.fullscreenEnabled is: ", document.fullscreenEnabled || document.mozFullScreenEnabled || document.webkitFullscreenEnabled || document.msFullscreenEnabled);
+}
