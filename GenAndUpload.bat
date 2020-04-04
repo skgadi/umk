c7c5ea301a33f6f43305d5ff23eea647db3a6873
Suresh Kumar Gadi <skgadi@users.noreply.github.com>
2020-04-04 02:06:07 -0600

    Generating and pushing into server. Also deploys functions

--- GenAndUpload.bat
+++ GenAndUpload.bat
@@ -4,6 +4,7 @@ xcopy .\docs-dev\css\fonts .\docs\css\fonts /y/s/q/i
 copy /y .\docs-dev\css\style.min.css .\docs\css\style.min.css
 copy /y .\docs-dev\script.min.js .\docs\script.min.js
 copy /y .\docs-dev\pre-scripts.min.js .\docs\pre-scripts.min.js
+copy /y .\docs-dev\app1.min.html .\docs\index.html
 
 git add .
 git commit -am "Generating and pushing into server. Also deploys functions"
