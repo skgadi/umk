597bad642ebb224b4e1631300dd3f261d924846e
Suresh Kumar Gadi <skgadi@users.noreply.github.com>
2020-10-22 10:57:12 -0500

    Generating and pushing into server. Also deploys functions

--- docs-dev/script/022-ad.js
+++ docs-dev/script/022-ad.js
@@ -3,7 +3,7 @@
         document.getElementById("skgadi-ad-area").style.display = "none";
         setTimeout(() => {
             document.getElementById("skgadi-ad-area").style.display = "block";
-        }, 60000);
+        }, 10*60000);
     }
     document.getElementById("skgadi-ad-area").addEventListener('click', setAnAd);
     setAnAd();
