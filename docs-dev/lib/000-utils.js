5a21bf6ece23b8abda3f1070429bf6dcc7d90965
Suresh Kumar Gadi <skgadi@users.noreply.github.com>
2023-10-17 15:22:49 -0600

    Automatic push

--- docs-dev/lib/000-utils.js
+++ docs-dev/lib/000-utils.js
@@ -266,34 +266,14 @@ const blockUtils = {
     //return out;
   },
   integrate: function (inItem, isFirstInEO = true) {
-    /*
-    //Initialize  the memory
-    if (!inItem.mem.length) {
-      inItem.mem.push(math.zeros(inItem.iv._data.length, inItem.iv._data[0].length));
-    }
-    //console.log("hey-->");
-    for (let i = inItem.mem.length; i < (intTypes[inItem.it].m + 1); i++) {
-      //console.log("-->"+i);
-      inItem.mem.unshift(math.zeros(inItem.mem[0]._data.length, inItem.mem[0]._data[0].length));
-    }
-    */
-
-
-    function addInps() {
-      if (!!inItem.inp) {
-        inItem.mem.unshift(inItem.inp);
-        //console.log(JSON.stringify(inItem.mem));
-      } else {
-        //TODO.....
-      }
-      /* else {
-              inItem.mem.push(math.zeros(inItem.iv._data.length, inItem.iv._data[0].length));
-            }*/
-      /*while (inItem.mem.length > (intTypes[inItem.it].m + 1)) {
-        inItem.mem.shift();
-        //console.log(inItem.mem.length);
-      }*/
-    }
+    // inItem.mem ----> memory
+    // inItem.it -----> Integration type
+    // inItem.iv -----> Initial value
+    // inItem.t ------> Simulation time
+    // inItem.inp ----> Input to integrate
+    // inItem.out ---> Previous output
+    // inItem.pt -----> Previous time
+    // inItem.isFr ---> is forward
 
     function getOuts() {
       //console.log(JSON.stringify(inItem.mem));
@@ -305,13 +285,9 @@ const blockUtils = {
           let h = (inItem.t - inItem.pt[0]) * ((!!intTypes[inItem.it].m) ? intTypes[inItem.it].m : 1);
           //console.log(h);
           for (let i = 0; i < intTypes[inItem.it].a.length; i++) {
-            // console.log(JSON.stringify(intTypes[inItem.it].b[i]));
-            // console.log(JSON.stringify(intTypes[inItem.it].c[i]));
-            // console.log(JSON.stringify(inItem.mem[math.round(intTypes[inItem.it].c[i] * intTypes[inItem.it].m)]));
             let calcRes = math.dotMultiply(h, math.dotMultiply(intTypes[inItem.it].a[i], inItem.mem[i]));
             if (!i) {
               out = calcRes;
-              //console.log(JSON.stringify(out));
             } else {
               out = math.add(out, calcRes);
             }
@@ -327,43 +303,7 @@ const blockUtils = {
         }
       }
     }
-    // inItem.mem ----> memory
-    // inItem.it -----> Integration type
-    // inItem.iv -----> Initial value
-    // inItem.t ------> Simulation time
-    // inItem.inp ----> Input to integrate
-    // inItem.out ---> Previous output
-    // inItem.pt -----> Previous time
-    // inItem.isFr ---> is forward
-    /*
-    if (inItem.isFr[0] === 0) {
-      if (!!inItem.inp) {
-        inItem.isFr[0] = true;
-      } else {
-        inItem.isFr[0] = false;
-      }
-    }*/
-    //console.log(inItem.isFr);
-    addInps();
     getOuts();
-    /*if (inItem.isFr[0]) {
-        addInps();
-        getOuts();
-      } else {
-        getOuts();
-        addInps();
-      }*/
-    //console.log(h);
-    //console.log(inItem.it);
-    //console.log(JSON.stringify(inItem.mem));
-    //inItem.mem = inItem.mem.slice(-(intTypes[inItem.it].m + 1));
-    //console.log(JSON.stringify(inItem.mem));
-    //console.log(inItem.mem.length);
-    //console.log(intTypes[inItem.it].m + 1);
-    //console.log(JSON.stringify(inItem.mem));
     inItem.pt[0] = inItem.t;
-    //console.log(inItem.t);
-    //console.log(JSON.stringify(inItem.out[0]._data));
-    //return out;
   }
 }
