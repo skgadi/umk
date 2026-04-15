2a34748acc1874baa40effccc5e5f6ce1ee4f3a5
Suresh Kumar Gadi <skgadi@users.noreply.github.com>
2026-04-15 09:41:25 -0600

    Generating and pushing into server.

--- firmware/esp32DoItDevKitV1/src/gsk_io_encoder.cpp
+++ firmware/esp32DoItDevKitV1/src/gsk_io_encoder.cpp
@@ -1,23 +1,20 @@
 #include "main.h"
 
-
-GSK_IO_ENCODER::GSK_IO_ENCODER (int id, int* pins):GSK_IO_PROPERTIES(id, pins) {
-  this->pins = new int[2];
-  this->pins[0] = pins[0];
-  this->pins[1] = pins[1];
-  reset();
+GSK_IO_ENCODER::GSK_IO_ENCODER(int id, int* pins)
+    : GSK_IO_PROPERTIES(id, pins) {
+    this->pins = new int[2];
+    this->pins[0] = pins[0];
+    this->pins[1] = pins[1];
+    reset();
 }
 
 void GSK_IO_ENCODER::reset() {
-  encoder = new ESP32Encoder();
-  ESP32Encoder::useInternalWeakPullResistors=puType::up;
-  encoder->attachHalfQuad(pins[0], pins[1]);
-  encoder->clearCount(); 
+    encoder = new ESP32Encoder();
+    ESP32Encoder::useInternalWeakPullResistors = puType::up;
+    encoder->attachFullQuad(pins[0], pins[1]);
+    encoder->clearCount();
 }
 
-void GSK_IO_ENCODER::loop() {
-  DATA_OUT[this->id] = encoder->getCount();
-}
+void GSK_IO_ENCODER::loop() { DATA_OUT[this->id] = encoder->getCount(); }
 
-void GSK_IO_ENCODER::setVal(nlohmann::json a) {
-}
+void GSK_IO_ENCODER::setVal(nlohmann::json a) {}
